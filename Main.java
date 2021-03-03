1c52af1684ca20b7568aec1f147404bc4ab59b06
mayerj3 <mayerj3@mail.uc.edu>
2021-03-03 02:19:53 +0000

    shifted student class left that caused error

--- Main.java
+++ Main.java
@@ -9,28 +9,27 @@ class Main {
 
     System.out.println("student1's name is " + student1.getName() + " and their GPAis " + student1.getGPA() + " and their major is " + student1.getMajor());
   }
+}
+class Student {  
+  String name;  
+  double gpa;  
+  String major;
 
-  class Student {  
-    String name;  
-    double gpa;  
-    String major;
-
-    String getName() {    
-      return name;  
-    }  
-    double getGPA() {    
-      return gpa;  
-    }  
-    String getMajor() {    
-      return major;  
-    }  
-    void setName(String theName) {    
-      name = theName;  
-    }  void setGPA(double theGPA) {    
-      gpa = theGPA;  
-    }  
-    void setMajor(String theMajor) {    
-      major = theMajor; 
-    }
+  String getName() {    
+    return name;  
+  }  
+  double getGPA() {    
+    return gpa;  
+  }  
+  String getMajor() {    
+    return major;  
+  }  
+  void setName(String theName) {    
+    name = theName;  
+  }  void setGPA(double theGPA) {    
+    gpa = theGPA;  
+  }  
+  void setMajor(String theMajor) {    
+    major = theMajor; 
   }
 }
